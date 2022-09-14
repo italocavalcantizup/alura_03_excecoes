381e3935a3d2a1c1d2fd92721cd589decd1fd934
Italo Cavalcanti <italo.cavalcanti@zup.com.br>
2022-09-14 15:48:16 -0300

    aprendendo sobre excecoes, testando Arithmetic e NullPointer

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -5,19 +5,27 @@ public class Fluxo {
 		metodo1();
 		System.out.println("Fim do main");
 	}
-	
+
 	public static void metodo1() {
 		System.out.println("Início do método 1");
-		metodo2();
-		System.out.println("Fim do método 1");		
+		try {
+			metodo2();			
+		} catch (ArithmeticException | NullPointerException e) { 
+			System.out.println("Exception " + e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println("Fim do método 1");
 	}
-	
+
 	public static void metodo2() {
 		System.out.println("Início do método 2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
+			int a = 1 / 0;
+			Conta c = null;
+			c.deposita();
 		}
-		System.out.println("Fim do método 2");		
+		System.out.println("Fim do método 2");
 	}
-	
+
 }
